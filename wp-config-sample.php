4c5c6b73ddc2f79f7442f413a95b8eedd988be7c
Sidd <sidd19_87@yahoo.com>
2018-04-10 18:18:43 +0530

    Update wp-config-sample.php
    DB Username change

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -23,7 +23,7 @@
 define('DB_NAME', 'database_name_here');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'test');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'password_here');
